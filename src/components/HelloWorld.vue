b4f0a2e7a58ef211d113051ce7c2bed41fd1506d
Erkan <erkan7830667@gmail.com>
2022-04-20 00:38:18 +0300

    orjinal

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <h1>{{ msg }}</h1>
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,
+          sint.
+        </h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
